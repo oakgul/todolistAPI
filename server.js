8508cd2caacc281e7b8ef90bf5abed0b17e083e1
Orhan Akgül <orhan.akgull@gmail.com>
2022-07-27 12:05:09 +0300

    hotfix

--- server.js
+++ server.js
@@ -11,17 +11,9 @@ connectDatabase();
 
 const app = express();
 app.use(express.json());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    next();
-});
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/todos', todos);
-
-
 
 app.listen(PORT, () => {
     console.log('Server running...');
