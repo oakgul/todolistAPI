070fce10702a044165f37bf3d5e56373da0f3301
Orhan Akgül <orhan.akgull@gmail.com>
2022-07-27 11:52:30 +0300

    cors error 2

--- server.js
+++ server.js
@@ -13,22 +13,17 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/api/todos', todos);
 
-const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-  
-  app.use(cors(corsOpts));
+
 
 
 
