19194f9ea45fb537b799e7da6916da85ae6a5698
Orhan Akgül <orhan.akgull@gmail.com>
2022-07-27 11:54:25 +0300

    cors error 3

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const todos = require("./routers/todos");
 const connectDatabase = require("./helpers/connectDB");
+const cors = require("cors");
+
 
 dotenv.config({
     path: "./config/env/config.env"
@@ -10,18 +12,11 @@ dotenv.config({
 connectDatabase();
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-app.use('/api/todos', todos);
 
 
 
